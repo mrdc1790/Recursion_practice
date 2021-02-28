a45cb6e413b271c32180b982fc3f7d208d3b0ffa
mrdc1790 <padam257@gmail.com>
2021-02-28 12:00:44 -0500

    factorial easy recursion

--- src/factorial.java
+++ src/factorial.java
@@ -1,19 +1,23 @@
 import java.util.stream.IntStream;
+import java.util.Scanner;
 
 public class factorial {
 
     public static void main(String args[]){
-        int recursionFact = factorialUsingRecursion(5);
-        System.out.println(recursionFact);
-
-        int loopsFact = factorialUsingForLoops(5);
-        System.out.println(loopsFact);
-
-        int streamsFact = factorialUsingStreams(5);
-        System.out.println(streamsFact);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter value to enter: ");
+        int factInput = input.nextInt();
+        int outputRecursion = factorialUsingRecursion(factInput);
+        int outputLoops = factorialUsingForLoops(factInput);
+        int outputStreams = factorialUsingStreams(factInput);
+        System.out.print("For factorials using Recursion: "+outputRecursion+"\nFor factorials using loops: "+outputLoops+"\nFor factorials using streams: "+outputStreams);
     }
 
     public static int factorialUsingForLoops(int n){
+        long startTime = System.nanoTime();
+        methodToTime();
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)
         int fact = 1;
         for (int i = 2; i <= n; i++)
             fact = fact * i;
